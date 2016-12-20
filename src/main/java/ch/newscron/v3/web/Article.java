67d0faca673830a178f47b35866fa826376959fd
Elia Palme <elia.palme@newscron.com>
2016-12-20 08:39:00 +0100

    fixed issue with long term url

--- src/main/java/ch/newscron/v3/web/Article.java
+++ src/main/java/ch/newscron/v3/web/Article.java
@@ -1,6 +1,7 @@
 package ch.newscron.v3.web;
 
 import ch.newscron.data.article.v2.ArticleFactory;
+import ch.newscron.data.article.v2.ArticleFactoryIFace;
 import ch.newscron.extractor.StructuredArticle;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,9 +19,19 @@ public class Article {
     @RequestMapping(value = "/topic/{topicId}/article/{articleId}", method = RequestMethod.GET)
     public String article(@PathVariable Long articleId) {
 
-        StructuredArticle article = ArticleFactory.getInstance().getArticle(articleId);
+        ArticleFactory articleFactory = ArticleFactory.getInstance();
+        StructuredArticle article = articleFactory.getArticle(articleId);
 
-        return "redirect:" + article.getUrl();
+        if (article == null) {
+            ArticleFactoryIFace.LongTermArticlesURLs articleURL = articleFactory.getLongtermArticleURL(articleId);
+            if (articleURL != null) {
+                return "redirect:" + articleURL.url;
+            }
+        } else {
+            return "redirect:" + article.getUrl();
+        }
+
+        return "Article not found";
     }
 
 
