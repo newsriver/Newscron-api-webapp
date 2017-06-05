410f3e3005ac4ff720540925015fe27d83018e75
Elia Palme <palme@NB-AMBP-157.local>
2017-06-05 18:03:20 +0200

    successful merge

--- app/src/app/app.routing.ts
+++ app/src/app/app.routing.ts
@@ -2,12 +2,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { CategoryComponent } from './category/category.component';
 import { FeaturedComponent } from './featured/featured.component';
 import { DigestsListComponent } from './digests-list/digests-list.component';
+import { CategoryConfigComponent } from './config/category-config/category-config.component';
+import { ConfigComponent } from './config/config.component';
+import { MainContentComponent } from './main-content/main-content.component';
+
+
 
 export const AppRoutes: Routes = [
-  { path: 'category/:id/:name', component: CategoryComponent },
-  { path: 'top', component: FeaturedComponent },
+  {
+    path: '', component: MainContentComponent,
+    children: [
+      { path: '', component: DigestsListComponent },
+      { path: 'top', component: FeaturedComponent },
+      { path: 'category/:id/:name', component: CategoryComponent }
+    ]
+  },
+  {
+    path: 'config', component: ConfigComponent,
+    children: [
+      { path: 'category/:id', component: CategoryConfigComponent }
+    ]
+  },
   { path: '**', redirectTo: '/', pathMatch: 'full' },
-  { path: '', component: DigestsListComponent }
 ];
 
 
