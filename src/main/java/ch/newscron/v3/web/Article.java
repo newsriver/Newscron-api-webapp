ec51af62da6e44e1de9dcde773aa3c25daf67695
Elia Palme <elia.palme@newscron.com>
2017-12-03 11:42:49 +0100

    readability enabler redirect + cookie

--- src/main/java/ch/newscron/v3/web/Article.java
+++ src/main/java/ch/newscron/v3/web/Article.java
@@ -4,11 +4,18 @@ import ch.newscron.data.article.v2.ArticleFactory;
 import ch.newscron.data.article.v2.ArticleFactoryIFace;
 import ch.newscron.extractor.StructuredArticle;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by eliapalme on 06.12.16.
@@ -17,8 +24,36 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class Article {
 
+    final DateFormat dateFormatDate = new SimpleDateFormat("dd MMMM ' | ' HH:mm a");
 
     @RequestMapping(value = "/topic/{topicId}/article/{articleId}", method = RequestMethod.GET)
+    public ModelAndView article(ModelMap model, @PathVariable Long articleId, @CookieValue(value = "readbility", required = false) Long readbility) {
+
+        ArticleFactory articleFactory = ArticleFactory.getInstance();
+        StructuredArticle article = articleFactory.getArticle(articleId);
+
+        if (article == null) {
+            ArticleFactoryIFace.LongTermArticlesURLs articleURL = articleFactory.getLongtermArticleURL(articleId);
+            if (articleURL != null) {
+                //return ResponseEntity.status(HttpStatus.FOUND).header("Location", articleURL.url).body(null);
+                model.addAttribute("attribute", "redirectWithRedirectPrefix");
+                return new ModelAndView("redirect:" + article.getUrl(), model);
+            } else {
+                //if the URL is not present in the LongTermArticleURL table, this Newscron URL is gone.
+                //return ResponseEntity.status(HttpStatus.GONE).body(null);
+                throw new HttpClientErrorException(HttpStatus.GONE);
+            }
+        } else if (readbility != null && readbility.equals(articleId)) {
+            return readability(article, model);
+        }
+        model.addAttribute("attribute", "redirectWithRedirectPrefix");
+        return new ModelAndView("redirect:" + article.getUrl(), model);
+        //return ResponseEntity.status(HttpStatus.FOUND).header("Location", article.getUrl()).body(null);
+
+
+    }
+
+    /*    @RequestMapping(value = "/topic/{topicId}/article/{articleId}", method = RequestMethod.GET)
     public ResponseEntity<String> article(@PathVariable Long articleId) {
 
         ArticleFactory articleFactory = ArticleFactory.getInstance();
@@ -35,7 +70,44 @@ public class Article {
         } else {
             return ResponseEntity.status(HttpStatus.FOUND).header("Location", article.getUrl()).body(null);
         }
-    }
 
+
+    }*/
+
+
+    private ModelAndView readability(StructuredArticle article, ModelMap model) {
+        String url = "";
+
+        int imageHO = 0;
+        int imageWO = 0;
+        boolean hasImage = false;
+        String text = null;
+        if (article != null) {
+            url = article.getUrl();
+            text = article.getHtml();
+            if (text == null || text.isEmpty()) {
+                article = null;
+            } else {
+                if (article.getImageID() > 0 && article.getImageWidth() >= 320) {
+                    imageWO = article.getImageWidth();
+                    imageHO = article.getImageHeight();
+                    hasImage = true;
+                }
+            }
+        }
+
+        model.addAttribute("url", url);
+        model.addAttribute("title", article.getTitle());
+        model.addAttribute("text", text);
+        model.addAttribute("id", article.getArticleID());
+        model.addAttribute("imageWO", imageWO);
+        model.addAttribute("imageHO", imageHO);
+        model.addAttribute("hasImage", hasImage);
+        model.addAttribute("publicationDate", dateFormatDate.format(new Date(article.getPublicationDateGMT())));
+        model.addAttribute("publisher", article.getPublisher());
+
+
+        return new ModelAndView("readability", model);
+    }
 
 }
--- src/main/java/ch/newscron/v3/web/Readability.java
+++ src/main/java/ch/newscron/v3/web/Readability.java
@@ -1,16 +1,11 @@
 package ch.newscron.v3.web;
 
-import ch.newscron.data.article.v2.ArticleFactory;
-import ch.newscron.extractor.StructuredArticle;
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by eliapalme on 30.11.17.
@@ -20,45 +15,18 @@ import java.util.Date;
 @Controller
 public class Readability {
 
-    private static final Logger log = Logger.getLogger(Readability.class);
-    final DateFormat dateFormatDate = new SimpleDateFormat("dd MMMM ' | ' HH:mm a");
-
-
+    //To redability view is provided by the Article. The Article view typically redirects the user to the original URL.
+    //However if a readbility cookie is set by the Readability request the Article view will display the redability mode.
+    //This mechanism is set in place to allow users to share the readability url. In this case the URL will not show the
+    //readability mode since the cookie is not present.
     @RequestMapping("/readability/{topicId}/{articleId}")
-    public String readability(Model model, @PathVariable Long topicId, @PathVariable Long articleId) {
+    public String article(@PathVariable Long topicId, @PathVariable Long articleId, HttpServletResponse response) {
+        Cookie cookie = new Cookie("readbility", "" + articleId);
+        cookie.setPath("/");
+        cookie.setMaxAge(300);
+        response.addCookie(cookie);
 
-        String url = "";
-        StructuredArticle article = ArticleFactory.getInstance().getArticle(articleId);
+        return "redirect:" + "/topic/" + topicId + "/article/" + articleId;
 
-        int imageHO = 0;
-        int imageWO = 0;
-        boolean hasImage = false;
-        String text = null;
-        if (article != null) {
-            url = article.getUrl();
-            text = article.getHtml();
-            if (text == null || text.isEmpty()) {
-                article = null;
-            } else {
-                if (article.getImageID() > 0 && article.getImageWidth() >= 320) {
-                    imageWO = article.getImageWidth();
-                    imageHO = article.getImageHeight();
-                    hasImage = true;
-                }
-            }
-        }
-
-        model.addAttribute("url", url);
-        model.addAttribute("title", article.getTitle());
-        model.addAttribute("text", text);
-        model.addAttribute("id", article.getArticleID());
-        model.addAttribute("imageWO", imageWO);
-        model.addAttribute("imageHO", imageHO);
-        model.addAttribute("hasImage", hasImage);
-        model.addAttribute("publicationDate", dateFormatDate.format(new Date(article.getPublicationDateGMT())));
-        model.addAttribute("publisher", article.getPublisher());
-
-
-        return "readability";
     }
 }
